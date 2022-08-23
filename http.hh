132718b11583d136ec24022fddda8c4e51a2a90d
domino <domino.r33d@gmail.com>
2022-08-22 19:03:35 -0700

    Adds HTTP Data Structure
    We need data structure to organize the HTTP data. This adds it for POST
    and GET since these are the main two that we will currently be using.

---
+++ http.hh
@@ -0,0 +1,18 @@
+#include <string>
+#include <unordered_map>
+
+enum http_type { get, post };
+
+struct http_request {
+  http_type type;
+  std::string url;
+};
+
+struct http_get : http_request {
+  std::unordered_map<std::string, std::string> args;
+};
+
+struct http_post : http_request {
+  int fd;
+  std::string content_type;
+};
