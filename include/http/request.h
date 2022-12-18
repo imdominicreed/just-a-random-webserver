a5b3e445699b74e76adc5c80feb0cee4c896715f
Evan Ugarte <36345325+evanugarte@users.noreply.github.com>
2022-12-18 11:17:05 -0800

    Add function to convert http method to domino::http::Method enum (#17)

--- include/http/request.h
+++ include/http/request.h
@@ -1,4 +1,5 @@
 #include <string>
+#include <string_view>
 #include <unordered_map>
 
 #ifndef HTTP_REQUEST_H_
@@ -6,7 +7,19 @@
 namespace domino {
 namespace http {
 
-enum Method { kGet, kPost };
+enum Method { kGet, kPost, kUnsupported };
+
+std::unordered_map<std::string, Method> string_to_method_map{};
+
+inline Method StringToMethod(std::string_view method_as_string) {
+  if (method_as_string == "GET") {
+    return Method::kGet;
+  } else if (method_as_string == "POST") {
+    return Method::kPost;
+  } else {
+    return Method::kUnsupported;
+  }
+}
 
 class Request {
  public:
